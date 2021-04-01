3c31d9db1869c2567703a3a5cbc2f84f46f00ca2
Andrea Parodi <andrea@parro.it>
2021-04-01 12:35:48 +0200

    dont cd on immediates run

--- schedule-run.sh
+++ schedule-run.sh
@@ -17,7 +17,10 @@ export DEPS=$SLURM_SUBMIT_DIR/PRG/deps/out;
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DEPS/lib
 export NETCDF=$DEPS
 
-cd $SLURM_SUBMIT_DIR
+if [[ $SLURM_SUBMIT_DIR != '' ]]; then 
+  echo STARTING IN $SLURM_SUBMIT_DIR
+  cd $SLURM_SUBMIT_DIR
+fi
 
 ulimit -s unlimited
 ./wrfda-runner -p DA .
