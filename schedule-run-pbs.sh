69ccfb436e7aebc74eaeee87afa7bda78f4dffc7
Andrea Parodi <andrea@parro.it>
2021-06-24 10:39:12 +0200

    fixes

--- schedule-run-pbs.sh
+++ schedule-run-pbs.sh
@@ -8,20 +8,26 @@
 ml iccifort/2020.1.217;
 ml impi/2019.7.217-iccifort-2020.1.217
 
+if [[ $PBS_O_WORKDIR != '' ]]; then 
+  WORKDIR=$PBS_O_WORKDIR
+else
+  WORKDIR=$PWD
+fi
+
+cd $WORKDIR
+
+echo STARTING IN $WORKDIR
+
+
 export SAVED_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SAVED_LD_LIBRARY_PATH:$DEPS/lib
-export DEPS=$PBS_O_WORKDIR/PRG/deps/out;
+export DEPS=$WORKDIR/PRG/deps/out;
 export NETCDF=$DEPS
-
-if [[ $PBS_O_WORKDIR != '' ]]; then 
-  echo STARTING IN $PBS_O_WORKDIR
-  cd $PBS_O_WORKDIR
-fi
 
 ulimit -s unlimited
 ./wrfda-runner -p DA .
 
-export DEPS_CDO=$PBS_O_WORKDIR/PRG/cdo;
+export DEPS_CDO=$WORKDIR/PRG/cdo;
 export LD_LIBRARY_PATH=$SAVED_LD_LIBRARY_PATH:$DEPS_CDO/lib
 export PATH=$PATH:$DEPS_CDO/bin
 ./scripts/delivery.sh
