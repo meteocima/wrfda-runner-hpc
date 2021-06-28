78244640ea84a40396c52235bebfc4a3350adc06
Andrea Parodi <andrea@parro.it>
2021-06-28 08:16:05 +0200

    changed slurm script to regrid results

--- schedule-run-slurm.sh
+++ schedule-run-slurm.sh
@@ -7,26 +7,33 @@
 #SBATCH --ntasks-per-node=28
 #SBATCH --mail-type=all
 #SBATCH --mail-user=andrea.parodi@cimafoundation.org
-#SBATCH --time=02:40:00
+#SBATCH --time=04:40:00
 #SBATCH --propagate=ALL
 #SBATCH --clusters=cm2
 #SBATCH --partition=cm2_std
 #SBATCH --qos=cm2_std
 
-export DEPS=$SLURM_SUBMIT_DIR/PRG/deps/out;
+if [[ $SLURM_SUBMIT_DIR != '' ]]; then 
+  WORKDIR=$SLURM_SUBMIT_DIR
+else
+  WORKDIR=$PWD
+fi
+
+cd $WORKDIR
+
+echo STARTING IN $WORKDIR
+
+
 export SAVED_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+export DEPS=$WORKDIR/PRG/deps/out;
 export LD_LIBRARY_PATH=$SAVED_LD_LIBRARY_PATH:$DEPS/lib
 export NETCDF=$DEPS
-
-if [[ $SLURM_SUBMIT_DIR != '' ]]; then 
-  echo STARTING IN $SLURM_SUBMIT_DIR
-  cd $SLURM_SUBMIT_DIR
-fi
 
 ulimit -s unlimited
 ./wrfda-runner -p DA .
 
-export DEPS_CDO=$SLURM_SUBMIT_DIR/PRG/cdo;
+export DEPS_CDO=$WORKDIR/PRG/cdo;
 export LD_LIBRARY_PATH=$SAVED_LD_LIBRARY_PATH:$DEPS_CDO/lib
+export PATH=$PATH:$DEPS_CDO/bin
 ./scripts/delivery.sh
-
