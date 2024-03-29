4dea5e901c084b0714b4d5c3b2b62f08c3d87836
Andrea Parodi <andrea@parro.it>
2024-03-29 10:04:07 +0100

    move scripts/delivery-vars.sh to delivery.sh

--- scripts/delivery.sh
+++ scripts/delivery.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+SCRIPTDIR=$PWD/scripts
+PRG=/mnt/proj2/dd-20-34/BUILD-NOCOMP
+NCO=$PRG/nco/
+DEPS=$PRG/deps/out;
+
 RH_EXPR="RH2=100*(PSFC*Q2/0.622)/(611.2*exp(17.67*(T2-273.15)/((T2-273.15)+243.5)))"
 RAINSUM_EXPR="RAINSUM=RAINNC+RAINC"
-SCRIPTDIR=$PWD/scripts
 
 CFG=`head -n 1 inputs/arguments.txt`
-OLD_IFS=$IFS
 IFS='-' read -ra DOMAIN <<< $CFG
-IFS=$OLD_IFS
 
+export LD_LIBRARY_PATH=$NCO/lib:$DEPS/lib
+export PATH=$PATH:$NCO/bin
+
+module purge
+ml GSL/2.7-GCC-10.3.0 
+ml UDUNITS/2.2.28-GCCcore-10.3.0 
+ml netCDF/4.8.0-gompi-2021a
+ml iccifort/2020.1.217
+ml impi/2019.9.304-iccifort-2020.1.217
+ml CDO/2.1.1-gompi-2021a
 
 if [[ $DOMAIN == "france" ]]; then
 	NUMDOMAIN=02
@@ -46,22 +58,27 @@ function regrid_date() {
 	    return
 	fi
 
-	rm *.nc || echo no previous regridded files found
+	rm *.nc *.2 *.3 *.ncks.tmp|| echo no previous regridded files found
 
 	auxfiles=`ls -fd auxhist23_d${NUMDOMAIN}_*`
 	
 	for auxf in $auxfiles; do
 		echo regridding $auxf
-		cdo setreftime,2000-01-01,00:00:00 -setcalendar,standard -remapbil,$SCRIPTDIR/$DOMAIN-cdo-d${NUMDOMAIN}-grid.txt -selgrid,1 $auxf regrid-$auxf.nc
+		ncks -A -v XLONG_U,XLAT_U,XLONG_V,XLAT_V wrfout_d03_* $auxf
+		ncks -O -x -v P_PL,U_PL,T_PL,Q_PL,V_PL,GHT_PL,S_PL,RH_PL,TD_PL,C1H,C2H,C1F,C2F,C3H,C4H,C3F,C4F $auxf $auxf.2
+		cdo -remapbil,$SCRIPTDIR/$DOMAIN-cdo-d${NUMDOMAIN}-grid.txt -selgrid,1,2,3 $auxf.2 $auxf.3
+		date=$(basename $auxf | cut -c 15-24)
+		time=$(basename $auxf | cut -c 26-34)
+   		echo Fixing time for $(basename $auxf) to $date $time 
+		cdo -O settaxis,$date,$time $auxf.3 regrid-$auxf.nc
 	done
-
 
 	# Merge all files into one that contains all simulation hours
 	cdo -v mergetime regrid* raw-${RUNDATE}.nc
 	
 	# Calculate RH variable
 	cdo -L -setrtoc,100,1.e99,100 -setunit,"%" -expr,$RH_EXPR raw-${RUNDATE}.nc rh-${RUNDATE}.nc
-
+	
 	# Merge source file and RH file
 	cdo -v merge raw-${RUNDATE}.nc rh-${RUNDATE}.nc lexis-$DOMAIN-${RUNDATE}.nc
 
@@ -82,13 +99,8 @@ while read d; do
 	if [[ $i != 0 ]]; then
 		export hours=`echo $d | cut -c 12-13`
   		export date=`echo $d | cut -c 1-8`
-  		#echo "line n.$i: date=$date hours=$hours"
-  		if [[ $hours == 48 ]]; then
- 	  		regrid_date $date $root/$date/wrf00
-			move_aux $date $root/$date/wrf00
-		else
-			move_aux $date $root/$date/wrf00
-  		fi
+		regrid_date $date $root/$date/wrf00
+		move_aux $date $root/$date/wrf00
   	fi
  	(( i=i+1 ))
 done < inputs/arguments.txt
